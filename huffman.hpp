ba95b7302e343dd67814927eb087e9ca48d61f19
Saksham-999 <saksham6624@student.ku.edu.np>
2026-02-24 11:22:33 +0545

    declarations done in hpp file

---
+++ huffman.hpp
@@ -0,0 +1,73 @@
+#ifndef HUFFMAN_HPP
+#define HUFFMAN_HPP
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <iomanip>
+
+using namespace std;
+
+
+//Huffman Tree Node (Binary Tree Node)
+struct HuffmanNode {
+    char data;           // Character stored 
+    int  freq;           // Frequency of character
+    HuffmanNode* left;   //left child
+    HuffmanNode* right;  //right child
+
+    HuffmanNode(char d, int f);
+};
+
+//Min-Heap Comparator (for Priority Queue)
+//Greedy algorithm(pick two nodes with lowest frequency)
+struct Compare {
+    bool operator()(HuffmanNode* a, HuffmanNode* b);
+};
+//  HUFFMAN TREE CLASS
+class HuffmanTree {
+private:            
+    HuffmanNode* root;   //stores starting node of tree (for tree traversal) 
+    unordered_map<char, string> codes;    // stores char  -> binary code (during compression)
+    unordered_map<string, char> reverse;  // stores binary code -> char (during decompression)
+    //unordered_map is a C++ standard library container which stores stores key-value pairs
+
+
+    void generateCodes(HuffmanNode* node, const string& code);  //recursively traverse tree and generate huffman codes
+    void freeTree(HuffmanNode* node);     //deletes dynamically allocated nodes
+    void printTree(HuffmanNode* node, int space, int gap); //displays tree(for debugging)
+
+public:
+    HuffmanTree();  //Initializes tree.
+    ~HuffmanTree();  //Automatically frees memory
+
+    unordered_map<char, int> buildFrequencyTable(const string& text); //reads text and counts and stored using unordered_map<char,int>
+    void   build(const unordered_map<char, int>& freq); //creates huffman tree using priority queue and greedy algorithm 
+    string encode(const string& text);  //converts text to binary string
+    string decode(const string& bitStr);  //converts binary to original text
+    void   printCodeTable();  //displays generated codes (just for demonstration)
+    void   printTree();   //displays tree 
+
+    //getter functions used (allow controlled access to private data)
+    unordered_map<char, string>& getCodes(); //provides huffman encoding table for compression.
+    unordered_map<string, char>& getReverse(); //returns binary codes back into characters.
+    HuffmanNode* getRoot();  //returns a pointer to the root node (which gives access to Huffman tree structure)
+};
+
+//file input/output (compress /decompress)
+void saveCompressed(const string& filename,
+                    const unordered_map<char, int>& freqTable,
+                    const string& encoded);
+
+void loadCompressed(const string& filename,
+                    unordered_map<char, int>& freqTable,
+                    string& encoded);
+
+void compressFile  (const string& inputFile,  const string& outputFile);
+void decompressFile(const string& inputFile,  const string& outputFile);
+
+#endif
